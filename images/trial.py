c7ad0dd807ab163174e3c56d763aaa543e995a73
Aryan Patil <125805392+aaryan-exe@users.noreply.github.com>
2024-11-26 12:45:08 +0530

    Update

--- images/trial.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk, Button, Label
-
-root = Tk()
-label = Label(root, text="Click here:")
-label.pack(side="left", padx=5)
-button = Button(root, text="Submit")
-button.pack(side="left", padx=5)
-root.mainloop()
